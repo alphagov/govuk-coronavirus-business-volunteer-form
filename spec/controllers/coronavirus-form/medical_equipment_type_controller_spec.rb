ed8aaaadadf4f46f39f6294261ef7d8ebce60c1c
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-03-20 17:09:38 +0000

    Add medical device questions branch
    This adds four questions that a user will see if they select
    Yes in response to the question Do you have medical equipment to offer?
    
    There is additional logic on the product details controller,
    since a user can enter multiple products if they click Yes
    in response to the question Do you have additional products to offer?
    
    We need to be able to edit these products at the end, so they
    are assigned IDs which will enable users to modify particular
    products.
    
    There is no limit on the number of products a user can add.
    If we move to using a db-backed store we should set a limit.
    
    This encompases questions 1.1 - 1.4, and should link to question
    2.

---
+++ spec/controllers/coronavirus-form/medical_equipment_type_controller_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe CoronavirusForm::MedicalEquipmentTypeController, type: :controller do
+  let(:current_template) { "coronavirus_form/medical_equipment_type" }
+  let(:session_key) { :medical_equipment_type }
+
+  describe "GET show" do
+    it "renders the form" do
+      get :show
+      expect(response).to render_template(current_template)
+    end
+  end
+
+  describe "POST submit" do
+    let(:selected) { ["Personal protection equipment", "Testing equipment"] }
+    it "sets session variables" do
+      post :submit, params: { medical_equipment_type: selected }
+
+      expect(session[session_key]).to eq selected
+    end
+
+    it "redirects to next step" do
+      post :submit, params: { medical_equipment_type: selected }
+
+      expect(response).to redirect_to(
+        "/coronavirus-form/are-you-a-manufacturer",
+      )
+    end
+
+    it "redirects to check your answers if check your answers already seen" do
+      session[:check_answers_seen] = true
+      post :submit, params: { medical_equipment_type: selected }
+
+      expect(response).to redirect_to("/coronavirus-form/check-your-answers")
+    end
+
+    it "validates any option is chosen" do
+      post :submit, params: { medical_equipment_type: [] }
+
+      expect(response).to render_template(current_template)
+    end
+
+    context "when Other option is selected" do
+      it "validates the Other option description is provided" do
+        post :submit, params: { medical_equipment_type: ["Other", "Personal protection equipment"] }
+
+        expect(response).to render_template(current_template)
+      end
+
+      it "adds the other option description to the session" do
+        post :submit, params: {
+          medical_equipment_type: ["Other", "Personal protection equipment"],
+          medical_equipment_type_other: "Demo text",
+        }
+
+        expect(response).to redirect_to(
+          "/coronavirus-form/are-you-a-manufacturer",
+        )
+        expect(session[session_key]).to eq ["Other", "Personal protection equipment"]
+        expect(session[:medical_equipment_type_other]).to eq "Demo text"
+      end
+    end
+
+    it "validates a valid option is chosen" do
+      post :submit, params: {
+        medical_equipment_type: ["<script></script", "invalid option", "Personal protection equipment"],
+      }
+
+      expect(response).to render_template(current_template)
+    end
+  end
+end
