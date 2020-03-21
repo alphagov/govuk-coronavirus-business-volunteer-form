e12388ac3a954e17f29699d5deeed7a7e789bc22
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-03-21 08:35:52 +0000

    Add question 4 about offering transport
    This adds question 4, which branches into asking
    about what kind of transport or offering space.
    
    https://trello.com/c/b0M4OBsx/25-implement-question-4-offer-transport

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,9 @@ Rails.application.routes.draw do
   get "/coronavirus-form/offer-advice" => "coronavirus_form/offer_advice#show"
   post "/coronavirus-form/offer-advice" => "coronavirus_form/offer_advice#submit"
 
+  get "/coronavirus-form/what-kind-of-transport" => "coronavirus_form/transport_type#show"
+  post "/coronavirus-form/what-kind-of-transport" => "coronavirus_form/transport_type#submit"
+
   # Check answers page
   get "/coronavirus-form/check-your-answers" => "coronavirus_form/check_answers#show"
   post "/coronavirus-form/check-your-answers" => "coronavirus_form/check_answers#submit"
