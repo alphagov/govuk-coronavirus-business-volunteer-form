a31821c99183846ab05f852625c7375370a777dc
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-03-23 13:37:36 +0000

    Remove "Can you offer food" question

--- spec/controllers/coronavirus_form/hotel_rooms_controller_spec.rb
+++ spec/controllers/coronavirus_form/hotel_rooms_controller_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe CoronavirusForm::HotelRoomsController, type: :controller do
 
     it "redirects to next step for a permitted response" do
       post :submit, params: { hotel_rooms: selected }
-      expect(response).to redirect_to(coronavirus_form_offer_food_path)
+      expect(response).to redirect_to(coronavirus_form_offer_transport_path)
     end
 
     it "redirects to check your answers if check your answers previously seen" do
