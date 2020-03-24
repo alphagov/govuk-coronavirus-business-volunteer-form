41a5306cf7c800ebc33bb97758de2f3a318a5e10
Alex <alex-ju@users.noreply.github.com>
2020-03-24 08:50:01 +0000

    Compress assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,13 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  # Rather than use a CSS compressor, use Sass-Rails to perform compression
+  config.sass.style = :compressed
+  config.sass.line_comments = false
+
+  # Compress JavaScript
+  config.assets.js_compressor = :uglifier
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
