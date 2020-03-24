7b092d336b2aff52230080024f5fb6b46c6670d1
Alex <alex-ju@users.noreply.github.com>
2020-03-24 09:53:36 +0000

    Fix Apply cache control in production #71
    Sorry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  if ENV["RAILS_SERVE_STATIC_FILES"].present?
     config.public_file_server.enabled = true
     config.public_file_server.headers = {
       "Cache-Control" => "public, s-maxage=31536000, max-age=31536000",
