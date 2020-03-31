812d046cf5cc3c49a4f0da7683e7193fef3b41bd
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-03-31 14:23:55 +0100

    Add question for number of hotel rooms
    Prompts user to enter the number of hotel rooms they have available if
    they state they have rooms available.

---
+++ spec/controllers/coronavirus_form/hotel_rooms_number_controller_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe CoronavirusForm::HotelRoomsNumberController, type: :controller do
+  let(:current_template) { "coronavirus_form/hotel_rooms_number" }
+  let(:session_key) { :hotel_rooms_number }
+
+  describe "GET show" do
+    it "renders the form when first question answered" do
+      session["medical_equipment"] = "Yes"
+      get :show
+      expect(response).to render_template(current_template)
+    end
+
+    it "redirects to first question when first question not answered" do
+      get :show
+      expect(response).to redirect_to({
+        controller: "medical_equipment",
+        action: "show",
+      })
+    end
+  end
+
+  describe "POST submit" do
+    let(:selected) { "100" }
+
+    it "sets session variables" do
+      post :submit, params: { hotel_rooms_number: selected }
+      expect(session[session_key]).to eq selected
+    end
+
+    it "redirects to check your answers if check your answers previously seen" do
+      session[:check_answers_seen] = true
+      post :submit, params: { hotel_rooms_number: selected }
+
+      expect(response).to redirect_to(check_your_answers_path)
+    end
+
+    it "validates a value is entered" do
+      post :submit, params: { hotel_rooms_number: "" }
+
+      expect(response).to render_template(current_template)
+    end
+  end
+end
