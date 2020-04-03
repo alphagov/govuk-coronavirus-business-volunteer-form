c095788203ce326ae10c64f58ea40fe19c1d56fb
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-04-03 11:42:30 +0100

    Add descriptions fields to offer space type
    Now a description field is required for each type of space
    that is offered.
    
    There is also a general description field.
    
    https://trello.com/c/3UKYWxkd/7-business-form-content-change-size-of-space

--- app/controllers/coronavirus_form/offer_space_type_controller.rb
+++ app/controllers/coronavirus_form/offer_space_type_controller.rb
@@ -1,21 +1,20 @@
 # frozen_string_literal: true
 
 class CoronavirusForm::OfferSpaceTypeController < ApplicationController
-  TEXT_FIELDS = %w(offer_space_type_other).freeze
+  OPTIONS = I18n.t("coronavirus_form.questions.#{controller_name}.options")
+  ALLOWED_VALUES = OPTIONS.map { |_, item| item.dig(:label) }
+  TEXT_FIELDS = [:general_space_description].freeze
+  DESCRIPTION_FIELDS = OPTIONS.map { |_, item| item.dig(:description, :id) }.freeze
 
   def submit
     offer_space_type = Array(params[:offer_space_type]).map { |item| strip_tags(item).presence }.compact
-    offer_space_type_other = strip_tags(params[:offer_space_type_other]).presence
     session[:offer_space_type] = offer_space_type
-    session[:offer_space_type_other] = description(:offer_space_type_other, :other, offer_space_type)
+    session[:offer_space_type_other] = description(:other, offer_space_type)
+    session[:warehouse_space_description] = description(:warehouse_space, offer_space_type)
+    session[:office_space_description] = description(:office_space, offer_space_type)
+    session[:general_space_description] = strip_tags(params[:general_space_description]).presence
 
-    invalid_fields = validate_field_response_length(controller_name, TEXT_FIELDS) +
-      validate_checkbox_field(
-        controller_name,
-        values: offer_space_type,
-        allowed_values: I18n.t("coronavirus_form.questions.#{controller_name}.options").map { |_, item| item.dig(:label) },
-        other: offer_space_type_other,
-      )
+    invalid_fields = validate_fields(offer_space_type)
 
     if invalid_fields.any?
       flash.now[:validation] = invalid_fields
@@ -29,9 +28,55 @@ class CoronavirusForm::OfferSpaceTypeController < ApplicationController
 
 private
 
-  def description(field, parent_field, checkboxes)
-    if selected_field?(parent_field, checkboxes)
-      strip_tags(params[field]).presence
+  def validate_fields(offer_space_type)
+    [
+      validate_field_response_length(controller_name, TEXT_FIELDS),
+      validate_checkbox_field(controller_name, values: offer_space_type, allowed_values: ALLOWED_VALUES),
+      validate_mandatory_text_fields(controller_name, TEXT_FIELDS),
+      validate_description_fields(offer_space_type),
+      validate_descriptions_response_length(offer_space_type),
+    ].flatten.compact
+  end
+
+  def validate_descriptions_response_length(_selected_options)
+    overlong_descriptions.map do |(option_id, option)|
+      {
+        field: option.dig(:description, :id).to_s,
+        text: t("coronavirus_form.questions.#{controller_name}.options.#{option_id}.description.custom_length_error"),
+      }
+    end
+  end
+
+  def overlong_descriptions
+    filter_options do |_field_id, option|
+      val = session[option.dig(:description, :id)]
+      val.present? && val.length > 1000
+    end
+  end
+
+  def filter_options(&predicate)
+    OPTIONS.to_a.select { |(field_id, option)| predicate.call(field_id, option) }.to_h
+  end
+
+  def validate_description_fields(selected_options)
+    missing_descriptions(selected_options).map do |field_id, option|
+      {
+        field: option.dig(:description, :id).to_s,
+        text: t("coronavirus_form.questions.#{controller_name}.options.#{field_id}.description.error_message"),
+      }
+    end
+  end
+
+  def missing_descriptions(selected_options)
+    filter_options do |field_id, option|
+      description_id = option.dig(:description, :id).to_sym
+      selected_field?(field_id, selected_options) && session[description_id].blank?
+    end
+  end
+
+  def description(field, checkboxes)
+    if selected_field?(field, checkboxes)
+      strip_tags(params[OPTIONS.dig(field, :description, :id)]).presence
     else
       ""
     end
--- spec/controllers/coronavirus_form/offer_space_type_controller_spec.rb
+++ spec/controllers/coronavirus_form/offer_space_type_controller_spec.rb
@@ -24,21 +24,30 @@ RSpec.describe CoronavirusForm::OfferSpaceTypeController, type: :controller do
 
   describe "POST submit" do
     let(:selected) { ["Warehouse space", "Office space"] }
+    let(:params) do
+      {
+        offer_space_type: ["Warehouse space", "Office space", "Other"],
+        warehouse_space_description: "200msq",
+        offer_space_type_other: "500sqm",
+        office_space_description: "400msq",
+        general_space_description: "I have extra space of 1000msq",
+      }
+    end
     it "sets session variables" do
-      post :submit, params: { offer_space_type: selected }
+      post :submit, params: params
 
-      expect(session[session_key]).to eq selected
+      expect(session.to_h).to eq params.deep_stringify_keys
     end
 
     it "redirects to next step" do
-      post :submit, params: { offer_space_type: selected }
+      post :submit, params: params
 
       expect(response).to redirect_to(expert_advice_type_path)
     end
 
     it "redirects to check your answers if check your answers already seen" do
       session[:check_answers_seen] = true
-      post :submit, params: { offer_space_type: selected }
+      post :submit, params: params
 
       expect(response).to redirect_to("/check-your-answers")
     end
@@ -51,14 +60,41 @@ RSpec.describe CoronavirusForm::OfferSpaceTypeController, type: :controller do
       end
 
       it "adds the other option description to the session" do
-        post :submit, params: {
+        post :submit, params: params.merge(
           offer_space_type: ["Other", "Office space"],
           offer_space_type_other: "A really big garden.",
-        }
+        )
 
         expect(response).to redirect_to(expert_advice_type_path)
         expect(session[session_key]).to eq ["Other", "Office space"]
         expect(session[:offer_space_type_other]).to eq "A really big garden."
+      end
+    end
+
+    described_class::OPTIONS.each do |(option_id, option)|
+      context "when the option #{option_id} is selected" do
+        description_id = option.dig(:description, :id).to_sym
+
+        context "when the option #{option_id} is not provided" do
+          let(:acceptable_params) {
+            params.merge(
+              offer_space_type: params[:offer_space_type].reject { |type|
+                type == option.dig(:label)
+              },
+            ).except(description_id)
+          }
+          it "doesn't require the description field #{description_id}" do
+            post :submit, params: acceptable_params
+            expect(response).to redirect_to(expert_advice_type_path)
+          end
+        end
+
+        context "when the corresponding description field #{description_id} is not provided" do
+          it "won't accept the input" do
+            post :submit, params: params.except(description_id)
+            expect(response).to render_template(current_template)
+          end
+        end
       end
     end
 
@@ -76,14 +112,13 @@ RSpec.describe CoronavirusForm::OfferSpaceTypeController, type: :controller do
 
     it "redirects to check your answers if check your answers previously seen" do
       session[:check_answers_seen] = true
-      post :submit, params: { offer_space_type: selected }
+      post :submit, params: params
 
       expect(response).to redirect_to(check_your_answers_path)
     end
 
-    described_class::TEXT_FIELDS.each do |field|
+    (described_class::TEXT_FIELDS + described_class::DESCRIPTION_FIELDS).each do |field|
       it "validates that #{field} is 1000 or fewer characters" do
-        params = { offer_space_type: ["Other", "Office space"] }
         params[field] = SecureRandom.hex(1001)
         post :submit, params: params
 
