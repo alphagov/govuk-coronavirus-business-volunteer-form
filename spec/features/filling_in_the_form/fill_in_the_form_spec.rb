031fe3758930e32b47461940f031d6bb5e1f2cda
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2020-04-09 13:11:24 +0100

    Convert static strings to I18N
    Initially, Feature tests were added with static strings.  To make the code more
    mintainable and less brittle - these needed to be converted to I18N strings.

--- spec/features/filling_in_the_form/fill_in_the_form_spec.rb
+++ spec/features/filling_in_the_form/fill_in_the_form_spec.rb
@@ -32,18 +32,18 @@ RSpec.feature "Fill in the form" do
   scenario "Ensure the privacy notice page is visible" do
     visit privacy_path
 
-    expect(page).to have_content("Privacy")
+    expect(page).to have_content(I18n.t("privacy_page.title"))
   end
 
   scenario "Ensure the accessibility statement page is visible" do
     visit accessibility_statement_path
 
-    expect(page).to have_content("Accessibility statement")
+    expect(page).to have_content(I18n.t("accessibility_statement.title"))
   end
 
   scenario "Ensure the session expired page is visible" do
     visit session_expired_path
 
-    expect(page).to have_content("Your session has ended due to inactivity")
+    expect(page).to have_content(I18n.t("session_expired.title"))
   end
 end
