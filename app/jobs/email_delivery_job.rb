f3fbbe2b283db13331dba7617091206f0683c573
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-04-30 09:28:34 +0100

    Do not retry a bad request
    If we make a bad request to Notify, we don't want to retry the job since
    we'll be making the same bad request again.  Therefore a mailer job will
    now be discarded when a bad request is made.

--- app/jobs/email_delivery_job.rb
+++ app/jobs/email_delivery_job.rb
@@ -3,6 +3,8 @@ class EmailDeliveryJob < ActionMailer::DeliveryJob
 
   queue_as :mailers
 
+  discard_on Notifications::Client::BadRequestError
+
   retry_on(Notifications::Client::RequestError,
            wait: :exponentially_longer,
            attempts: 5)
