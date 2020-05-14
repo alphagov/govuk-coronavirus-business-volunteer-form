69357cea6c5a18f50b41b37a7585bfeecd587715
Alex <alex-ju@users.noreply.github.com>
2020-05-14 17:39:34 +0100

    Import component scripts individually

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,20 @@
 //= require jquery
-// = require govuk/all.js
-//= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/modules
 //= require govuk_publishing_components/lib/cookie-functions
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/character-count
+//= require govuk_publishing_components/components/checkboxes
+//= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/radio
 //= require analytics
 //= require cookies
 window.CookieSettings.start()
-window.GOVUKFrontend.initAll()
+
 if (window.GOVUK.analyticsInit) {
   window.GOVUK.analyticsInit()
 }
+
+$(document).ready(function () {
+  window.GOVUK.modules.start()
+})
