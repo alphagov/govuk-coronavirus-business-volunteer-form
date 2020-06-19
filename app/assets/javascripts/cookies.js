21b710a4b74f92a1f8ec707b83ad1ab48c8871fd
Ian James <ian.james@digital.cabinet-office.gov.uk>
2020-06-19 17:35:48 +0100

    Lint and fix JavaScript
     - Ran the StandardJS linter and fixed all of the disagreements - mainly
       tweaking spacing and removing semicolons.
     - Added jQuery to the global namespace so the linter will recognise `$` as a
       variable.

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -3,8 +3,7 @@ window.GOVUK = window.GOVUK || {}
 function CookieSettings () {}
 
 CookieSettings.start = function () {
-
-  var cookieForm = document.querySelector('form[data-module=cookie-settings]');
+  var cookieForm = document.querySelector('form[data-module=cookie-settings]')
 
   if (cookieForm) {
     cookieForm.addEventListener('submit', this.submitSettingsForm)
@@ -21,11 +20,11 @@ CookieSettings.setInitialFormValues = function () {
   var currentConsentCookieJSON = JSON.parse(currentConsentCookie)
 
   // We don't need the essential value as this cannot be changed by the user
-  delete currentConsentCookieJSON["essential"]
+  delete currentConsentCookieJSON.essential
   // We don't need the campaigns/settings values as these aren't required by
   // the service.
-  delete currentConsentCookieJSON["campaigns"]
-  delete currentConsentCookieJSON["settings"]
+  delete currentConsentCookieJSON.campaigns
+  delete currentConsentCookieJSON.settings
 
   for (var cookieType in currentConsentCookieJSON) {
     var radioButton
@@ -43,21 +42,21 @@ CookieSettings.setInitialFormValues = function () {
 CookieSettings.submitSettingsForm = function (event) {
   event.preventDefault()
 
-  var formInputs = event.target.getElementsByTagName("input")
+  var formInputs = event.target.getElementsByTagName('input')
   var options = {}
 
-  for ( var i = 0; i < formInputs.length; i++ ) {
+  for (var i = 0; i < formInputs.length; i++) {
     var input = formInputs[i]
     if (input.checked) {
       var name = input.name.replace('cookies-', '')
-      var value = input.value === "on" ? true : false
+      var value = input.value === 'on'
 
       options[name] = value
     }
   }
 
   window.GOVUK.setConsentCookie(options)
-  window.GOVUK.setCookie('cookies_preferences_set', true, { days: 365 });
+  window.GOVUK.setCookie('cookies_preferences_set', true, { days: 365 })
 
   CookieSettings.showConfirmationMessage()
 
@@ -73,5 +72,5 @@ CookieSettings.showConfirmationMessage = function () {
 
   document.body.scrollTop = document.documentElement.scrollTop = 0
 
-  confirmationMessage.style.display = "block"
+  confirmationMessage.style.display = 'block'
 }
