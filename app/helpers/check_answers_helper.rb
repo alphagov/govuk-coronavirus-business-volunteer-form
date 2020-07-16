6014f1bf7fba3e1721f23ecca41df790ee9b9adb
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2020-07-16 14:06:37 +0100

    Add support methods
    Partials will be used to render each section, a method to define the
    sections (by name) and the order that they are to be rendered in is 
    therefore required.  It will then be possible to loop around the 
    sections list - in order - and display each section partial.
    
    Each section can be defined as a header and detail.  The detail for each
    section will be handled by the appropriate partial, but the header is
    identical in structure across all sections except for the name of the 
    section name itself.  A single method can handle the data required by
    all section headers.
    
    The default Rails inflection rules do not handle the pluralization of 
    'foot' - mistakenly returning 'foots'.  An inflection rule is required
    to handle this instance and correctly return 'feet' when appropriate.
    
    This commit adds the above changes.

--- app/helpers/check_answers_helper.rb
+++ app/helpers/check_answers_helper.rb
@@ -6,4 +6,36 @@ module CheckAnswersHelper
   def charge_text
     I18n.t("coronavirus_form.check_your_answers.charge")
   end
+
+  def question_items(question)
+    if session[question].is_a?(Array)
+      [{
+        field: I18n.t("coronavirus_form.questions.#{question}.title"),
+        value: render("govuk_publishing_components/components/list", {
+          visible_counters: true,
+          items: session[question],
+        }),
+      }]
+    else
+      [{
+        field: I18n.t("coronavirus_form.questions.#{question}.title"),
+        value: session[question],
+      }]
+    end
+  end
+
+  def sections
+    %w[
+      accommodation
+      transport
+      space
+      staff
+      care
+      services
+      other_support
+      location
+      business_details
+      contact_details
+    ]
+  end
 end
--- spec/helpers/check_answers_helper_spec.rb
+++ spec/helpers/check_answers_helper_spec.rb
@@ -1,4 +1,38 @@
 require "spec_helper"
 
 RSpec.describe CheckAnswersHelper, type: :helper do
+  include FormResponseHelper
+
+  let(:form_data) { valid_data }
+
+  describe "#question_items(question)" do
+    it "contains a string" do
+      session.merge!(form_data)
+
+      expect(helper.question_items("accommodation")).to eq(
+        [
+          {
+            field: I18n.t("coronavirus_form.questions.accommodation.title"),
+            value: form_data[:accommodation],
+          },
+        ],
+      )
+    end
+
+    it "contains an array" do
+      session.merge!(form_data)
+
+      expect(helper.question_items("location")).to eq(
+        [
+          {
+            field: I18n.t("coronavirus_form.questions.location.title"),
+            value: render("govuk_publishing_components/components/list", {
+              visible_counters: true,
+              items: form_data[:location],
+            }),
+          },
+        ],
+      )
+    end
+  end
 end
