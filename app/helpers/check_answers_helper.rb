d0ca233e76838aa621356bd2cd324f306d8b0aaa
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2020-07-16 13:53:55 +0100

    Add I18N strings
    The redesign requires slightly different text than is currently
    available in the en.yml file.  
    
    Instead of changing the existing text - which is appropriate to the 
    pages that these elements are on and so should not be changed - a new 
    section is added to specifically handle text appropriate to the 
    check_your_answers page.
    
    In order to shorten and simplify the I18N string look-up within the 
    check_your_answers_helper and reduce duplication, two helper methods are
    added.
    
    This commit adds the above changes.

--- app/helpers/check_answers_helper.rb
+++ app/helpers/check_answers_helper.rb
@@ -1,2 +1,9 @@
 module CheckAnswersHelper
+  def sections_scope
+    "coronavirus_form.check_your_answers.sections"
+  end
+
+  def charge_text
+    I18n.t("coronavirus_form.check_your_answers.charge")
+  end
 end
